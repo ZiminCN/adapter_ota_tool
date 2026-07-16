decffe659f46d9dc44fbd1d32f2e5b91a9659a0c
Violet-Zimin <jianming.zeng@directdrivetech.com>
2026-07-16 09:37:56 +0800

    trival: modified third-parts lib for python-can intelhex
    Signed-off-by: Violet-Zimin <jianming.zeng@directdrivetech.com>

--- adapter-ota-tool/can_tool/can_tool.py
+++ adapter-ota-tool/can_tool/can_tool.py
@@ -1,4 +1,5 @@
-import can
+from can.interface import Bus
+from can import Message
 from typing import List
 
 class CanTool:
@@ -10,14 +11,14 @@ class CanTool:
                         "extended": False   # 标准帧
                         },
                 ]
-                self.can_bus = can.Bus(interface='socketcan', channel='can0', fd=True, can_filters=ota_filters)
+                self.can_bus = Bus(interface='socketcan', channel='can0', fd=True, can_filters=ota_filters)
                 
         def __del__(self):
                 self.can_bus.shutdown()
                 return
                 
         def send_can_data(self, is_fd: bool, bitrate_switch: bool, can_id: int, can_data: List[int]):
-                can_message = can.Message(is_fd=is_fd, bitrate_switch=bitrate_switch, arbitration_id=can_id, data=can_data, is_extended_id=False)
+                can_message = Message(is_fd=is_fd, bitrate_switch=bitrate_switch, arbitration_id=can_id, data=can_data, is_extended_id=False)
                 self.can_bus.send(can_message, timeout=0.2)
                 
         def receive_can_data(self):
--- adapter-ota-tool/install_py_necessary_lib.sh
+++ adapter-ota-tool/install_py_necessary_lib.sh
@@ -3,4 +3,4 @@
 sudo apt update
 sudo apt install -y python3-pip
 
-pip3 install python-can[serial] numpy intelhex
+pip3 install python-can==4.6.1 numpy intelhex==2.3.0
